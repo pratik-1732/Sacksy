d5e97edd71d9796bfce244f5d0cf4c6bcfb9024f
Pratik <patilpratikp035@gmail.com>
2025-06-08 09:56:10 +0530

    8th june - created routes and required in app.js

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const db = require("./config/mongooseConnection");
 
+// router req
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
 const app = express();
 const PORT = 3000;
 
@@ -13,9 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.send("Hey");
-});
+// routers
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}...`);
